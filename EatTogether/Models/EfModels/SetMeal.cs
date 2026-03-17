71e9516a37582616b02031920e8d082b84723f03
楊 <yang0005111@gmail.com>
2026-03-17 10:29:54 +0800

    更改型別錯誤轉換問題

--- EatTogether/Models/EfModels/SetMeal.cs
+++ EatTogether/Models/EfModels/SetMeal.cs
@@ -42,4 +42,6 @@ public partial class SetMeal
     public bool IsRecommended { get; set; }
 
     public virtual ICollection<SetMealItem> SetMealItems { get; set; } = new List<SetMealItem>();
+
+    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
