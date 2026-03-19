22f58d9f169054550da3f1eb1c3a4b36e18b4c9b
Weiyan-99 <g2301149040@gmail.com>
2026-03-19 15:37:30 +0800

    Feature/articles 功能完成 (#79)
    * add 文章分類新增 ViewModel, Dto , 轉換擴充方法
    
    * add ArticleCategory Service/Repository
    
    * add ArticleCategoriesController
    
    * add 文章分類首頁 ViewModel, Dto, 轉換方法
    
    * modify ArticleCategoryRepository、ArticleService、ArticleCategoriesController
    
    * 更動article註冊位置
    
    * 暫存
    
    * add 文章分類編輯  ViewModel, Dto , modify擴充方法
    
    * doc修改
    
    * 檔案修正
    
    * 文章分類編輯-modify repo, service, controller, _EditPartial.cshtml
    
    * add 文章新增頁面  ViewModel, Dto , 擴充方法
    
    * 在 Program.cs 註冊IArticleRepository, ArticleRepository, ArticleService
    
    * add ArticlesController
    
    * 文章新增頁面更新
    
    * add 文章列表頁面 ViewModel, Dto , 擴充方法
    
    * 文章列表頁面 modify ArticleRepository, ArticleService, ArticlesController
    
    * 主檔合併
    
    * 檔案整合
    
    * add 文章預覽頁面 ViewModel, Dto , 擴充方法
    
    * 文章預覽改作文章編輯頁面
    add ViewModel, Dto , 擴充方法
    repo, service, controller 修改
    
    * 文章編輯頁更新
    
    * add 文章軟刪除(下架) + 草稿硬刪除
    
    * 活動表加上[IsAutoDiscount]>>0為手工判定，1為系統帶入，作為訂單結帳用途
    文章列表更新
    
    * rebase合併修正

--- EatTogether/Models/EfModels/Product.cs
+++ EatTogether/Models/EfModels/Product.cs
@@ -15,9 +15,11 @@ public partial class Product
 
     public int? SetMealId { get; set; }
 
+
     public virtual Dish Dish { get; set; }
 
-    public virtual ICollection<MemberFavorite> MemberFavorites { get; set; } = new List<MemberFavorite>();
+
+	public virtual ICollection<MemberFavorite> MemberFavorites { get; set; } = new List<MemberFavorite>();
 
     public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
 
