361d558c7260e85d7e00a2449065de633ec99ade
吳欣柔 <rrr20118@gmail.com>
2026-05-06 16:51:06 +0800

    新增PreOrderDetailId到OrderDetails(已重跑EFcore)

--- db/27_OrderDetails.sql
+++ db/27_OrderDetails.sql
@@ -1,12 +1,11 @@
-﻿-- =============================================
--- 27_OrderDetails.sql
--- 說明：將有效預約明細轉入正式成交明細表
--- =============================================
-
-USE EatTogetherDB;
+﻿USE EatTogetherDB;
 GO
 
--- 根據正式 Orders 表的紀錄轉入明細
+-- 暫時停用 FK 約束
+ALTER TABLE [dbo].[OrderDetails] NOCHECK CONSTRAINT [FK_OrderDetails_ParentDetail];
+GO
+
+-- Step 1：INSERT 所有有效明細，ParentDetailId 先帶入 PreOrderDetails 的舊 Id
 INSERT INTO [dbo].[OrderDetails] 
 (
     [OrderId], 
@@ -15,20 +14,32 @@ INSERT INTO [dbo].[OrderDetails]
     [UnitPrice], 
     [Qty], 
     [SubTotal],
-    [ParentDetailId]
+    [ParentDetailId],
+    [PreOrderDetailId]
 )
 SELECT 
-    O.[Id],            -- 指向正式 Orders 表的自增 Id
+    O.[Id],
     PD.[ProductId], 
     PD.[ProductName], 
     PD.[UnitPrice], 
     PD.[Qty], 
     PD.[SubTotal],
-    PD.[ParentDetailId]
+    PD.[ParentDetailId],  -- 暫存 PreOrderDetails 的舊 Id
+    PD.[Id]
 FROM [dbo].[PreOrderDetails] AS PD
 INNER JOIN [dbo].[Orders] AS O ON PD.[PreOrderId] = O.[PreOrderId]
-WHERE PD.[DoneOrCancel] = 1; -- 只轉入有效(完成或待作)的餐點，排除已取消(2)的餐點
+WHERE PD.[DoneOrCancel] = 1;
 GO
 
-USE EatTogetherDB;
+-- Step 2：換算 ParentDetailId 成 OrderDetails 的正確 Id
+UPDATE Child
+SET Child.[ParentDetailId] = Parent.[Id]
+FROM [dbo].[OrderDetails] AS Child
+INNER JOIN [dbo].[OrderDetails] AS Parent
+    ON Parent.[PreOrderDetailId] = Child.[ParentDetailId]
+WHERE Child.[ParentDetailId] IS NOT NULL;
+GO
+
+-- 重新啟用 FK 約束並驗證資料
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_ParentDetail];
 GO
